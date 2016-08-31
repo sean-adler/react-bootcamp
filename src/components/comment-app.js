d09e37304316a5d4a77e5619324bfb4ca7d7c4d7
Jeff McRiffey <jmcriffey@gmail.com>
2016-08-31 14:43:52 -0400

    initial commit

---
+++ src/components/comment-app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../styles/comment-app.css';
+
+class CommentApp extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello Comment App!
+            </div>
+        );
+    }
+}
+
+export default CommentApp;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CommentApp from './components/comment-app';
+
+ReactDOM.render(<CommentApp />, document.querySelector('.app'));

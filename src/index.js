88a55f4c4c720e35a5a457f84a4677251b93855d
Sean Adler <sean@counsyl.com>
2016-09-15 21:10:28 -0700

    Remove original

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'whatwg-fetch';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './styles/comment-app.css';
-import CommentApp from './components/comment-app';
-
-/* global document */
-ReactDOM.render(<CommentApp />, document.querySelector('.app'));
